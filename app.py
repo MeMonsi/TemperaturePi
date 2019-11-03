939751280e377461cdf9e20e2a17f441d7230ca8
TheWurstbrot <simon.grosnick@gmx.net>
2019-11-03 13:00:57 +0100

    Add live-chart to display temperature on a 10sec base

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from time import sleep, strftime, time
+from datetime import datetime
 from src.TemperatureSlave import TemperatureSlave
+import json
+import random
 
 app = Flask(__name__)
-tempeatureSlave = TemperatureSlave()
+slave = TemperatureSlave()
+random.seed()
 
 @app.route('/')
 def index():
@@ -12,7 +16,18 @@ def index():
 
 @app.route('/DummyTemperatureSlave')
 def DummyTemperatureSlave(): 
-    return str(tempeatureSlave.ReadTemperature())
+    return str(slave.ReadTemperature())
+
+@app.route('/chart-data')
+def chart_data():
+    def generate_random_data():        
+        while True:
+            json_data = json.dumps(
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': slave.ReadTemperature()})
+            yield f"data:{json_data}\n\n"
+            sleep(10)
+
+    return Response(generate_random_data(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
